87a3c20d362e814011d33a9cf7c57ccdd6ef1d72
ssssss789 <strachanlubo@gmail.com>
2024-10-28 11:19:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask
-import os  # Import the os module
+from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def home():
+@app.route('/')
+def index():
     return "Welcome to the Flask app!"
 
-@app.route("/heartbeat", methods=["GET"])
+@app.route('/heartbeat', methods=['POST'])
 def heartbeat():
-    return "App is running", 200
+    data = request.get_json()
+    timestamp = data.get('timestamp')
+    print(f"Received heartbeat at: {timestamp}")
+    return jsonify(message="App is running"), 200
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))

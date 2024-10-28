d486afc2e1031c3823e057fa9d9f73f3408eb7db
ssssss789 <strachanlubo@gmail.com>
2024-10-28 10:32:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def heartbeat():
     return "App is running", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))

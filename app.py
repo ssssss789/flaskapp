3ed3badb7b197b9eefcdfa53d903845be56f9958
ssssss789 <strachanlubo@gmail.com>
2025-02-24 19:07:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def index():
 
 # This variable will store the latest heartbeat timestamp
 latest_heartbeat = None
+latest_heartbeat2 = None
 
 @app.route('/heartbeat', methods=['POST', 'GET'])
 def heartbeat():
@@ -25,6 +26,22 @@ def heartbeat():
     elif request.method == 'GET':
         if latest_heartbeat:
             return jsonify(message="App is running", last_heartbeat=latest_heartbeat, status=200), 200
+        else:
+            return jsonify(message="No heartbeat received yet", status=200), 200
+
+@app.route('/heartbeat2', methods=['POST', 'GET'])
+def heartbeat2():
+    global latest_heartbeat2
+
+    if request.method == 'POST':
+        # Capture the current time when the heartbeat is received
+        latest_heartbeat2 = datetime.now().isoformat()
+        data = request.json
+        print("Received heartbeat data:", data)
+        return jsonify(message="Heartbeat received", timestamp=latest_heartbeat2, status=200), 200
+    elif request.method == 'GET':
+        if latest_heartbeat2:
+            return jsonify(message="App is running", last_heartbeat=latest_heartbeat2, status=200), 200
         else:
             return jsonify(message="No heartbeat received yet", status=200), 200
 
